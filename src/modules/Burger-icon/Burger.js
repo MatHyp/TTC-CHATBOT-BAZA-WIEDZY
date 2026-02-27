d9c1b780d683c45639a6658ff3e77aaca46a6790
windforce2007 <94519254+windforce2007@users.noreply.github.com>
2026-02-27 13:34:22 +0100

    some responsable for menu

--- src/modules/Burger-icon/Burger.js
+++ src/modules/Burger-icon/Burger.js
@@ -2,10 +2,14 @@ import style from "./Burger.module.css";
 import { useState } from "react";
 function Burger({DoAfterClick}) {
 	
-	//const [ShowPanel, setShowPanel] = useState(false);
-
+	const [BurgerCliked, setBurgerCliked] = useState(false);
+const DoAfterClickLocal=()=>
+{
+	setBurgerCliked(!BurgerCliked);
+	DoAfterClick();
+};
   return (
-	<div className={style.Burger} onClick={DoAfterClick}>
+	<div className={`${style.Burger} ${BurgerCliked && style.BurgerCliked}`}onClick={DoAfterClickLocal}>
 		<div className={style.Burgerline}></div>
 		<div className={style.Burgerline}></div>
 		<div className={style.Burgerline}></div>
--- src/modules/SideBar/SideBar.js
+++ src/modules/SideBar/SideBar.js
@@ -9,10 +9,19 @@ const changeState = () => {
 };
   return (
 	<>
-		<Burger DoAfterClick={changeState}/>
+		
 		<div className={`${className} ${ShowPanel  ? style.sideBarLocal : style.sideBarLocalDezactive }`} >
-
+		<div className={style.Title}>
+			<h1>TTC Docktor</h1>
+			<Burger DoAfterClick={changeState}/>
 		</div>
+			<ul>
+				<li className={`${style.MenuItemActive} `} inerd={ShowPanel}>Home</li>
+				<li className={style.MenuItem}>Login</li>
+				<li className={style.MenuItem}>Api Info</li>
+			</ul>
+			
+	    </div>
   </>);
 }
 
